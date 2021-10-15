0f61f8df09473587b2fc60ee6ae7c05c4e6ef518
manas221 <manass246172@gmail.com>
2021-10-15 23:13:42 +0530

    Env setup and first file made

---
+++ Errors ,Algebraic and Transcedental roots/root_methods.jl
@@ -0,0 +1,21 @@
+### A Pluto.jl notebook ###
+# v0.16.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 916d8960-2ddd-11ec-39ca-319664fbd4cd
+md"""
+## Methods to calculate roots of transcedental equation
+"""
+
+# ╔═╡ d5be58bd-6718-47c7-983c-02a8120f5aca
+
+
+# ╔═╡ a13d0a44-7a1b-413b-aa1a-1ffb473a6a4e
+
+
+# ╔═╡ Cell order:
+# ╟─916d8960-2ddd-11ec-39ca-319664fbd4cd
+# ╠═d5be58bd-6718-47c7-983c-02a8120f5aca
+# ╠═a13d0a44-7a1b-413b-aa1a-1ffb473a6a4e
